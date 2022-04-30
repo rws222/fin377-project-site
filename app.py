68ffb22f96af6eb9c694ab4011d3fd44c86b4387
Ryan Schmid <65303191+rws222@users.noreply.github.com>
2022-04-30 18:45:44 -0400

    inital commit

---
+++ app.py
@@ -0,0 +1,34 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+    
+#     # Load current count
+#     f = open("count.txt", "r")
+#     count = int(f.read())
+#     f.close()
+
+#     # Increment the count
+#     count += 1
+
+#     # Overwrite the count
+#     f = open("count.txt", "w")
+#     f.write(str(count))
+#     f.close()
+
+#     # Render HTML with count variable
+#     return render_template("index.html", count=count)
+
+# if __name__ == "__main__":
+#     app.run()
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
---
+++ templates/README.md
@@ -0,0 +1,5 @@
+# Final Project Website
+
+Website is published here: https://rws222.github.io/fin377-final-project-site/
+
+The site is built using HTML, but the functionality of the project will be built with a jupyterlab notebook. This notebook will then be converted into HTML, and inserted into the website.
