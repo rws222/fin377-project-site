b3c44452e00376d4bc7293dc9648359e10588eb4
Ryan Schmid <65303191+rws222@users.noreply.github.com>
2022-05-01 13:55:32 -0400

    added working test ajax request

--- app.py
+++ app.py
@@ -24,11 +24,38 @@
 # if __name__ == "__main__":
 #     app.run()
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+
+
+@app.route('/results', methods=['POST', 'GET'])
+def get_results():
+   if request.method == "POST":
+      req = request.get_json()
+      
+      myans1 = req[0]['tag1']
+      myans1a = req[0]['tag1a']
+      myans2 = req[1]['tag2']
+      
+      # run all of our code
+   
+      # download graphs
+      
+      
+      results = [{'res_tag1': myans1, 'res_tag1a': myans1a},
+                 {'res_tag2': myans2}]
+      return jsonify(results)
+   return
+      
+      
+   
+
+
+
 if __name__ == '__main__':
    app.run()
